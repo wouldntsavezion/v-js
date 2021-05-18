73a009dc7a0f9e31e90071744e5d0aa3634ca5f1
Nicola Baribeau <n_baribeau@hotmail.com>
2021-05-17 22:48:08 -0400

    Flavour

--- server/auth/decrypt.ts
+++ server/auth/decrypt.ts
@@ -1,15 +1,15 @@
 import crypto from "crypto";
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
-const KEY = fs.readFileSync(path.resolve(__dirname, 'KEY'), 'hex');
+const KEY = fs.readFileSync(path.resolve(__dirname, "KEY"), "hex");
 
 // Encrypt / Decrypt courtesy of https://stackoverflow.com/a/60370205
 export default function decrypt(s:string) {
-	let textParts = s.split(':');
+	let textParts = s.split(":");
 	let shift = textParts.shift() as string;
-	let iv = Buffer.from(shift, 'hex');
-	let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+	let iv = Buffer.from(shift, "hex");
+	let encryptedText = Buffer.from(textParts.join(":"), "hex");
 	let decipher = crypto.createDecipheriv("aes-256-ctr", Buffer.from(KEY), iv);
 	let decrypted = decipher.update(encryptedText);
 	decrypted = Buffer.concat([decrypted, decipher.final()]);
--- server/auth/encrypt.ts
+++ server/auth/encrypt.ts
@@ -1,8 +1,8 @@
 import crypto from "crypto";
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
-const KEY = fs.readFileSync(path.resolve(__dirname, 'KEY'), 'hex');
+const KEY = fs.readFileSync(path.resolve(__dirname, "KEY"), "hex");
 
 // Encrypt / Decrypt courtesy of https://stackoverflow.com/a/60370205
 export default function encrypt(s:string) {
@@ -10,5 +10,5 @@ export default function encrypt(s:string) {
 	let cipher = crypto.createCipheriv("aes-256-ctr", Buffer.from(KEY), iv);
 	let encrypted = cipher.update(s);
 	encrypted = Buffer.concat([encrypted, cipher.final()]);
-	return iv.toString('hex') + ':' + encrypted.toString('hex');
+	return iv.toString("hex") + ":" + encrypted.toString("hex");
 }
